044fbb31778c32015ef2a5bcef02f802602e2d11
michielkikkert <michiel@kikkert.nl>
2025-05-24 12:10:35 +0200

    Cleanup

--- apps/signal-experiments/src/app/containers/step1/step1.component.ts
+++ apps/signal-experiments/src/app/containers/step1/step1.component.ts
@@ -1,13 +1,12 @@
-import { Component, effect, inject, Injector, linkedSignal, model } from '@angular/core';
+import { Component, effect, inject, Injector, model } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { httpResource, HttpResourceRef } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatButton } from '@angular/material/button';
-import { MatSlideToggle } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-step1',
-    imports: [CommonModule, FormsModule, MatButton, MatSlideToggle],
+    imports: [CommonModule, FormsModule, MatButton],
     templateUrl: './step1.component.html',
     styleUrl: './step1.component.css',
 })
@@ -20,7 +19,6 @@ export class Step1Component {
             : undefined
     );
     lazyResource: HttpResourceRef<any> | undefined;
-
 
     // So this is not using a linkedSignal - but it checks this.id() - if empty then it will return undefined for the URL - which doesn't make te request.
     // Nice! But not really as now the previous result dissappears on the resource, removing the first result from html..? I don't understand why it works like that.
